930a0986468111953bfbc4375f9f296ace0d41fe
Neds001 <NedsMamitag001@gmail.com>
2022-11-09 13:33:47 +0800

    update

--- update.php
+++ update.php
@@ -4,6 +4,7 @@
 	$sql="SELECT * FROM `users` WHERE id=$id";
 	$result=mysqli_query($con,$sql);
 	$row=mysqli_fetch_assoc($result);
+
 		$name=$row['name'];
 		$email=$row['email'];
 		$mobile=$row['mobile'];
@@ -36,7 +37,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Create</title>
+	<title>Update</title>
 	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
 </head>
 <body>
@@ -45,24 +46,27 @@
 		<form method="POST">
   			<div class="form-control">
     			<label>Name</label>
-   				<input type="text" class="form-control" placeholder="Enter your name" name="name" autocomplete="off" <?php echo $name ?>>
+   				<input type="text" class="form-control" placeholder="Enter your name" name="name"
+   				 autocomplete="off"
+   				 value = <?php echo $name;?>>
 
   			</div>
   			<div class="form-control">
     			<label>Email</label>
-   				<input type="email" class="form-control" placeholder="Enter your email" name="email" autocomplete="off"<?php echo $email ?>>
+   				<input type="email" class="form-control" placeholder="Enter your email" name="email" autocomplete="off" value=<?php echo $email;?>>
 
   			</div>
   			<div class="form-control">
     			<label>Mobile</label>
-   				<input type="text" class="form-control" placeholder="Enter your mobile number" name="mobile" autocomplete="off"<?php echo $mobile ?>>
+   				<input type="text" class="form-control" placeholder="Enter your mobile number" name="mobile" autocomplete="off" value=<?php echo $mobile;?>>
 
   			</div>
   			<div class="form-control">
     			<label>Password</label>
-   				<input type="text" class="form-control" placeholder="Enter your password" name="password" autocomplete="off"<?php echo $password ?>>
+   				<input type="text" class="form-control" placeholder="Enter your password" name="password" autocomplete="off"value=<?php echo $password;?>>
 
   			</div>
+
 
   			<button type="submit" class="btn btn-primary" name="submit">Update</button>
 
